4063821f699ddf3d798e91ef787d566f4b9cdfba
kukulim <beckon@interia.pl>
2020-08-09 16:56:32 +0200

    easy

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CodeWars
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            int n = 9119;
-            string nstring = n.ToString();
-            string wynik = String.Empty;
-            for (int i = 0; i < nstring.Length; i++)
-            {
-                Console.WriteLine(nstring[i]);
-                int buffor = (int)char.GetNumericValue(nstring[i]);
-                wynik += Math.Pow(buffor, 2).ToString();
-        }
-            int intwynik = Convert.ToInt32(wynik);
-        Console.WriteLine(intwynik);
+
         }
     }
 }
