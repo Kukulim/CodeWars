8dadbb48b08d67c4fbecd822501eed2ca46f9449
kukulim <beckon@interia.pl>
2020-09-09 13:21:33 +0200

    Rectangle into Squares good

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -9,16 +9,6 @@ namespace CodeWars
     {
         private static void Main()
         {
-            int lng = 5;
-            int wdth = 3;
-            Console.WriteLine(sqInRect(lng,wdth));
-        }
-        public static List<int> sqInRect(int lng, int wdth)
-        {
-
-            List<int> result = new List<int>();
-            // your code
-            return null;
         }
     }
 }
