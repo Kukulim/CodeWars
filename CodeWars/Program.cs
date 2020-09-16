bbe5f125139fc0d861d5535a30e157ed53df6c1d
kukulim <beckon@interia.pl>
2020-09-16 17:37:32 +0200

    Calculator half way

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,7 +10,71 @@ namespace CodeWars
         private static void Main()
         {
             //Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine(Evaluate("1 - 1+1"));
+        }
+        public static double Evaluate(string expression)
+        {
+            string Snumber = expression.Replace(" ", "");
+            List<object> list = new List<object>();
+            foreach (var item in Snumber)
+            {
+               list.Add(item);
+            }
+
+            while (list.Find(c => c.ToString() == "*") != null)
+            {
+                var result = 0;
+                var index = list.FindIndex(r => r.ToString() == "*");
+                result = int.Parse(list[index - 1].ToString()) * int.Parse(list[index + 1].ToString());
+                list[index] = result;
+                list.RemoveAt(index - 1);
+                list.RemoveAt(index);
+            }
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            while (list.Find(c => c.ToString() == "/") != null)
+            {
+                var result = 0;
+                var index = list.FindIndex(r => r.ToString() == "/");
+                result = int.Parse(list[index - 1].ToString()) / int.Parse(list[index + 1].ToString());
+
+                list[index] = result;
+                list.RemoveAt(index - 1);
+                list.RemoveAt(index);
+            }
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            while (list.Find(c=>c.ToString()=="-")!=null)
+                {
+                    var result = 0;
+                    var index = list.FindIndex(r => r.ToString() == "-");
+                    result = int.Parse(list[index - 1].ToString()) - int.Parse(list[index + 1].ToString());
+                list[index] = result;
+                list.RemoveAt(index - 1);
+                list.RemoveAt(index);
+            }
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            while (list.Find(c => c.ToString() == "+") != null)
+            {
+                var result = 0;
+                var index = list.FindIndex(r => r.ToString() == "+");
+                result = int.Parse(list[index - 1].ToString()) + int.Parse(list[index + 1].ToString());
+                list[index] = result;
+                list.RemoveAt(index - 1);
+                list.RemoveAt(index);
+            }
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            return double.Parse(list[0].ToString());
         }
     }
 }
